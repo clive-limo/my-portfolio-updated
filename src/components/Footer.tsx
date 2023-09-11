631e34711217d2cd29a9145b4592f6b12ec60041
Clive Limo <clivelimo84@gmail.com>
2023-09-11 17:32:36 +0300

    [fix] footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,7 @@ import { AppConfig } from '@/utils/AppConfig';
 export default function Footer() {
   return (
     <footer className="invisible mt-10 border-t border-zinc-500 py-8 text-center text-sm md:visible">
-      © Copyright {new Date().getFullYear()} {AppConfig.title}. Made with{' '}
-      <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>
+      © Copyright {new Date().getFullYear()} {AppConfig.title}.
     </footer>
   );
 }
